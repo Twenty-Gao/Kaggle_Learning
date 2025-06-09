b8e610187873ad10a1fffffbba974ca47dea4733
Lexus_Twenty <13811361+lexus-twenty@user.noreply.gitee.com>
2025-06-09 09:41:23 +0800

    gaoyu69941

--- DigitRecognizer/main.py
+++ DigitRecognizer/main.py
@@ -1,31 +1,18 @@
 # 这是一个示例 Python 脚本。
+import torch
+
 
 # 按 Shift+F10 执行或将其替换为您的代码。
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
 
-
+def relu(x):
+    x[x<0]=0
+    return x
 def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
     print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
 
-import pandas as pd
-
-# 假设数据集名为 'titanic.csv'
-data = pd.read_csv('sample_submission.csv')
-
-# 查看数据集的前五行
-print(data.head())
-# 检查缺失值
-print(data.isnull().sum())
-
-# 填补缺失值
-data['Age'].fillna(data['Age'].median(), inplace=True)
-
-# 删除不必要的列
-data.drop(['Ticket', 'Cabin'], axis=1, inplace=True)
-
-# 转换类别变量
-data = pd.get_dummies(data, columns=['Sex'], drop_first=True)
+    print(relu(torch.tensor([-1,2,-3,4])))
 
 
 # 按间距中的绿色按钮以运行脚本。
